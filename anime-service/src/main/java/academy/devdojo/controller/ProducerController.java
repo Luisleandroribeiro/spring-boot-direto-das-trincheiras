8fe03c36bc2bd430494bfaf0624695db0e945c51
Luisleandroribeiro <alemaole.luis@gmail.com>
2025-06-03 16:04:46 -0300

    video18: Decoupling domain requests and responses

--- anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
+++ anime-service/src/main/java/academy/devdojo/controller/ProducerController.java
@@ -1,6 +1,8 @@
 package academy.devdojo.controller;
 
 import academy.devdojo.domain.Producer;
+import academy.devdojo.request.ProducerPostRequest;
+import academy.devdojo.response.ProducerGetResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -8,36 +10,47 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+
 @Slf4j
 @RestController
-@RequestMapping ("/v1/producers")
+@RequestMapping("/v1/producers")
 public class ProducerController {
 
     @GetMapping
-    public List<Producer> listAll (@RequestParam (required = false) String name){
+    public List<Producer> listAll(@RequestParam(required = false) String name) {
         var producers = Producer.getProducers();
-        if (name==null) return producers;
+        if (name == null) return producers;
 
-        return producers.stream().filter(n->n.getName().equalsIgnoreCase(name)).toList();
+        return producers.stream().filter(n -> n.getName().equalsIgnoreCase(name)).toList();
     }
 
     @GetMapping("{id}")
-    public Producer findById (@PathVariable Long id){
+    public Producer findById(@PathVariable Long id) {
         return Producer.getProducers()
                 .stream()
-                .filter(producers->producers.getId().equals(id))
+                .filter(producers -> producers.getId().equals(id))
                 .findFirst().orElse(null);
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE, headers = "x-api-key")
-    public ResponseEntity<Producer> addProducer (@RequestBody Producer producer, @RequestHeader HttpHeaders headers){
-        log.info ("{}", headers);
-        producer.setId(ThreadLocalRandom.current().nextLong(1,100000));
+    public ResponseEntity<ProducerGetResponse> save(@RequestBody ProducerPostRequest producerPostRequest, @RequestHeader HttpHeaders headers) {
+        log.info("{}", headers);
+        Producer producer = Producer.builder()
+                .id(ThreadLocalRandom.current().nextLong(1, 100000))
+                .name(producerPostRequest.getName())
+                .createdAt(LocalDateTime.now())
+                .build();
+
         Producer.getProducers().add(producer);
-        var responseHeaders = new HttpHeaders();
-        responseHeaders.add("Authorization", "My Key");
-        return ResponseEntity.status(HttpStatus.CREATED).headers(responseHeaders).body(producer);
+
+        var response = ProducerGetResponse.builder()
+                .id(producer.getId())
+                .name(producer.getName())
+                .createAt(producer.getCreatedAt())
+                .build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- anime-service/src/main/java/academy/devdojo/domain/Producer.java
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -1,26 +1,28 @@
 package academy.devdojo.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@Builder
 public class Producer {
 
     private Long id;
     @JsonProperty("name")
     private String name;
+    private LocalDateTime createdAt;
     private static List<Producer> producers = new ArrayList<>();
     static {
-        var masashiKishimoto = new Producer(1L, "Masashi Kishimoto");
-        var aniplex = new Producer(2L, "Aniplex");
-        var a1Pictures = new Producer(3L, "A-1 Pictures");
+        var masashiKishimoto = Producer.builder().id(1L).name("Masashi Kishimoto").createdAt(LocalDateTime.now()).build();
+        var aniplex = Producer.builder().id(2L).name("Aniplex").createdAt(LocalDateTime.now()).build();
+        var a1Pictures = Producer.builder().id(3L).name("A-1 Pictures").createdAt(LocalDateTime.now()).build();
         producers.addAll(List.of(masashiKishimoto, aniplex, a1Pictures));
     }
 

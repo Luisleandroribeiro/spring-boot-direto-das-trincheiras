1c1b388f03637a20cba6aa28dd6148553466ad4d
Luisleandroribeiro <alemaole.luis@gmail.com>
2025-08-14 20:01:10 -0300

    video85: Exercício Spring Data Anime Service pt 05 - Entidade Producer

--- anime-service/src/main/java/academy/devdojo/producer/ProducerService.java
+++ anime-service/src/main/java/academy/devdojo/producer/ProducerService.java
@@ -34,10 +34,12 @@ public class ProducerService {
     }
 
     public void update(Producer producerToUpdate) {
-        var producer = findByIdOrThrowNotFound(producerToUpdate.getId());
-        producerToUpdate.setCreatedAt(producer.getCreatedAt());
+        assertProducerExists(producerToUpdate.getId());
         repository.save(producerToUpdate);
     }
 
+    public void assertProducerExists(Long id) {
+        findByIdOrThrowNotFound(id);
+    }
 
 }

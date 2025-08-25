c12e2099013a98a178297d4c5273d254b628cd4a
Luisleandroribeiro <alemaole.luis@gmail.com>
2025-08-25 13:53:28 -0300

    video94: Spring Data Jpa Many to Many 02

---
+++ user-service/src/main/java/academy/devdojo/service/UserProfileService.java
@@ -0,0 +1,22 @@
+package academy.devdojo.service;
+
+
+import academy.devdojo.domain.Profile;
+import academy.devdojo.domain.UserProfile;
+import academy.devdojo.exception.NotFoundException;
+import academy.devdojo.repository.ProfileRepository;
+import academy.devdojo.repository.UserProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserProfileService {
+    private final UserProfileRepository repository;
+
+   public List<UserProfile> findAll() {
+       return repository.findAll();
+   }
+}

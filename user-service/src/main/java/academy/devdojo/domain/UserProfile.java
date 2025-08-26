b61d659a65ecc682781b94ca9ab521e912e0ce0c
Luisleandroribeiro <alemaole.luis@gmail.com>
2025-08-26 20:16:43 -0300

    video99: JPA EntityGraphs

--- user-service/src/main/java/academy/devdojo/domain/UserProfile.java
+++ user-service/src/main/java/academy/devdojo/domain/UserProfile.java
@@ -11,6 +11,8 @@ import lombok.*;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@NamedEntityGraph(name = "UserProfile.fullDetails",
+        attributeNodes = {@NamedAttributeNode("user"), @NamedAttributeNode("profile")})
 
 public class UserProfile {
     @EqualsAndHashCode.Include
--- user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
+++ user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
@@ -2,6 +2,7 @@ package academy.devdojo.repository;
 
 
 import academy.devdojo.domain.UserProfile;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,4 +14,7 @@ public interface UserProfileRepository extends JpaRepository<UserProfile, Long> 
     @Query("SELECT up FROM UserProfile up join fetch up.user u join fetch up.profile p")
     List<UserProfile> retrieveAll();
 
+    //    @NamedEntityGraph(attributePaths = {"user", "profile"})
+    @EntityGraph("UserProfile.fullDetails")
+    List<UserProfile> findAll();
 }
